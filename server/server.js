b4b7c731cd87886720765d1ef933b337926d5d20
user <user@users-MacBook-Air.local>
2023-02-03 15:27:38 +0200

    Initial server setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,8 @@
-console.log("server");
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
