32f965c186c1ad80493e7fd83889934e349871d8
user <user@users-MacBook-Air.local>
2023-02-03 16:38:31 +0200

    Add veggie options to the model

--- server/models/guest.js
+++ server/models/guest.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const guestSchema = new mongoose.Schema({
   names: "",
   uniqueCode: "",
+  veggie: {},
 });
 
 const Guest = mongoose.model("Guest", guestSchema);
